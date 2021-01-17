bba15c4fcfded7a2093345710322b01efa14e99c
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-01-16 21:54:07 -0400

    Change sizes char/short/int

--- understanding-numbers/script.js
+++ understanding-numbers/script.js
@@ -1,16 +1,34 @@
-const size = 8;
-let state = {
+const state = {
     sign : 1,
+    size : 8,
+    bits : [],
 };
-const weights = [];
 
-for (let i=0; i<size; i++)
-    weights.push(2 ** i);
-const bits = new Array(size).fill(0);
+function populate(state) {
+    state.bits = new Array(state.size).fill(0);
+    populateWeights(state);
+    populateBits(state);
+    populateSwitchs(state);
+    populateValue(state);
+    initializeEncoding();
+}
 
-function populateWeights(size) {
-    for (let i=size-1; i>=0; i--) {
-        if (i==size-1)
+function setSize(s) {
+    state.size = Number(s);
+    state.sign = 1;
+    unpopulate();
+    populate(state);
+}
+
+function unpopulate() {
+    document.getElementById("weights").innerHTML = "";
+    document.getElementById("bits").innerHTML = "";
+    document.getElementById("switchs").innerHTML = "";
+}
+
+function populateWeights(state) {
+    for (let i=state.size-1; i>=0; i--) {
+        if (i==state.size-1)
             document.getElementById("weights").innerHTML +=
             `<div id="signWeight" class='value weightVal sign'>2<sup>${i}</sup></div>`;
         else
@@ -19,25 +37,25 @@ function populateWeights(size) {
     }
 }
 
-function populateBits(bits) {
-   for (let i=size-1; i>=0; i--) {
-        if (i==size-1)
+function populateBits(state) {
+   for (let i=state.size-1; i>=0; i--) {
+        if (i==state.size-1)
             document.getElementById("bits").innerHTML +=
-            `<div id='bit${i}' class='value bitVal sign'>${bits[i]}</div>`;
+            `<div id='bit${i}' class='value bitVal sign'>${state.bits[i]}</div>`;
         else
             document.getElementById("bits").innerHTML +=
-            `<div id='bit${i}' class='value bitVal'>${bits[i]}</div>`;
+            `<div id='bit${i}' class='value bitVal'>${state.bits[i]}</div>`;
     }
 }
 
-function populateSwitchs(size) {
-    for (let i=size-1; i>=0; i--) {
-        if (i==size-1)
+function populateSwitchs(state) {
+    for (let i=state.size-1; i>=0; i--) {
+        if (i==state.size-1)
             /* Add extra id to sign input swicth */
             document.getElementById("switchs").innerHTML += `
 <div class="checkButton">
   <label class="switch">
-    <input id="signInput" type="checkbox" onchange="swapBit(bits, ${i})">
+    <input id="signInput" type="checkbox" onchange="swapBit(state, ${i})">
     <span class="slider round"></span>
   </label>
 </div>`;
@@ -45,45 +63,45 @@ function populateSwitchs(size) {
             document.getElementById("switchs").innerHTML += `
 <div class="checkButton">
   <label class="switch">
-    <input type="checkbox" onchange="swapBit(bits, ${i})">
+    <input type="checkbox" onchange="swapBit(state, ${i})">
     <span class="slider round"></span>
   </label>
 </div>`;
     }
 }
 
-function swapBit(bits, i) {
-    bits[i] = (bits[i] + 1) % 2;
+function swapBit(state, i) {
+    state.bits[i] = (state.bits[i] + 1) % 2;
     const id = "bit" + String(i);
-    document.getElementById(id).innerText = String(bits[i]);
-    populateValue(bits, size, state);
+    document.getElementById(id).innerText = String(state.bits[i]);
+    populateValue(state);
 }
 
-function populateValue(bits, size, state) {
-    const val = getValue(bits, size, state);
+function populateValue(state) {
+    const val = getValue(state);
     document.getElementById("value").innerText = String(val);
 }
 
-function getValue(bits, size, state ) {
+function getValue(state) {
     let val = 0;
-    for (let i=0; i<size; i++) {
-        if (i == size -1)
-            val += state.sign * bits[i] * 2**i;
+    for (let i=0; i<state.size; i++) {
+        if (i == state.size -1)
+            val += state.sign * state.bits[i] * 2**i;
         else
-            val += bits[i] * 2**i;
+            val += state.bits[i] * 2**i;
     }
     return val;
 }
 
 function handleSwapEncoding() {
-    swapEncoding(bits, size, state);
+    swapEncoding(state);
 }
 
-function swapEncoding(bits, size, state) {
+function swapEncoding(state) {
     state.sign *= -1;
     const newWeight = state.sign > 0 ?
-          `2<sup>${size-1}` :
-          `-2<sup>${size-1}`;
+          `2<sup>${state.size-1}` :
+          `-2<sup>${state.size-1}`;
     document.getElementById("signWeight").innerHTML =
         newWeight;
     /* Change color */
@@ -100,16 +118,12 @@ function swapEncoding(bits, size, state) {
         document.getElementById("signInput").setAttribute('sign', '1');
 
     /* Update value */
-    populateValue(bits, size, state);
+    populateValue(state);
 }
 
 function initializeEncoding() {
     document.getElementById('encodeCheck').checked = false;
 }
 
-populateWeights(size);
-populateBits(bits);
-populateSwitchs(size);
-populateValue(bits, size, state);
-initializeEncoding();
+populate(state);
 
